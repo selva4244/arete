da56e91d22334e87c5620c1ea905c0d4e2d0338f
anusiyagwebtech <anusiyagwebtech@gmail.com>
2023-11-30 15:23:06 +0530

    First Comment

---
+++ assets/js/pop.js
@@ -0,0 +1,7 @@
+const popup = document.getElementById("popup-parent");
+const closemark = document.getElementById("intMark");
+closemark.addEventListener("click", ()=>{
+    popup.style.display = "none";
+})
+
+
